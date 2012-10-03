ce93ff19ac680a624dee9e9e6df19ca3ae99b994
Umesh Madan <umeshma@microsoft.com>
2012-10-03 15:53:25 -0700

    Health 2.0 Sample

---
+++ Samples/Demos/Health2.0/HelloHealthVault/HelloHealthVault/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloHealthVault
+//
+//  Created by Umesh Madan on 4/16/12.
+//  Copyright (c) 2012 Microsoft Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HVAppDelegate class]));
+    }
+}
