d4e6b6c6909e86810797537a6b8c6f693be09c8b
Michael Burford <miburf@microsoft.com>
2017-05-08 08:57:24 -0700

    Modernize 6

--- MHVLib/Types/MHVName.h
+++ MHVLib/Types/MHVName.h
@@ -1,15 +1,15 @@
 //
-//  MHVName.h
-//  MHVLib
+// MHVName.h
+// MHVLib
 //
-//  Copyright (c) 2017 Microsoft Corporation. All rights reserved.
+// Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,72 +21,63 @@
 #import "MHVVocab.h"
 
 @interface MHVName : MHVType
-{
-@private
-    NSString* m_full;
-    MHVCodableValue* m_title;
-    NSString* m_first;
-    NSString* m_middle;
-    NSString* m_last;
-    MHVCodableValue* m_suffix;
-}
 
-//-------------------------
+// -------------------------
 //
 // Data
 //
-//-------------------------
+// -------------------------
 //
 // (Required)
 //
-@property (readwrite, nonatomic, strong) NSString* fullName;
+@property (readwrite, nonatomic, strong) NSString *fullName;
 //
 // (Optional)
 // Vocabulary: name-prefixes
 //
-@property (readwrite, nonatomic, strong) MHVCodableValue* title;
+@property (readwrite, nonatomic, strong) MHVCodableValue *title;
 //
 // (Optional)
 //
-@property (readwrite, nonatomic, strong) NSString* first;
+@property (readwrite, nonatomic, strong) NSString *first;
 //
 // (Optional)
 //
-@property (readwrite, nonatomic, strong) NSString* middle;
+@property (readwrite, nonatomic, strong) NSString *middle;
 //
 // (Optional)
 //
-@property (readwrite, nonatomic, strong) NSString* last;
+@property (readwrite, nonatomic, strong) NSString *last;
 //
 // (Optional)
 // Vocabulary: name-suffixes
-@property (readwrite, nonatomic, strong) MHVCodableValue* suffix;
+@property (readwrite, nonatomic, strong) MHVCodableValue *suffix;
 
-//-------------------------
+// -------------------------
 //
 // Initializers
 //
-//-------------------------
--(id) initWithFirst:(NSString *) first andLastName:(NSString *) last;
--(id) initWithFirst:(NSString *) first middle:(NSString *) middle andLastName:(NSString *) last;
--(id) initWithFullName:(NSString *) name;
+// -------------------------
+- (instancetype)initWithFirst:(NSString *)first andLastName:(NSString *)last;
+- (instancetype)initWithFirst:(NSString *)first middle:(NSString *)middle andLastName:(NSString *)last;
+- (instancetype)initWithFullName:(NSString *)name;
 
-//-------------------------
+// -------------------------
 //
 // Methods
 //
-//-------------------------
--(BOOL) buildFullName;
+// -------------------------
+- (BOOL)buildFullName;
 
-+(MHVVocabIdentifier *) vocabForTitle;
-+(MHVVocabIdentifier *) vocabForSuffix;
++ (MHVVocabIdentifier *)vocabForTitle;
++ (MHVVocabIdentifier *)vocabForSuffix;
 
-//-------------------------
+// -------------------------
 //
 // Text
 //
-//-------------------------
+// -------------------------
 // Returns the full name
--(NSString *) toString;
+- (NSString *)toString;
 
 @end
--- MHVLib/Types/MHVPendingItem.h
+++ MHVLib/Types/MHVPendingItem.h
@@ -1,15 +1,15 @@
 //
-//  MHVPendingItem.h
-//  MHVLib
+// MHVPendingItem.h
+// MHVLib
 //
-//  Copyright (c) 2017 Microsoft Corporation. All rights reserved.
+// Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,18 +22,13 @@
 #import "MHVItemType.h"
 
 @interface MHVPendingItem : MHVType
-{
-@private
-    MHVItemKey* m_id;
-    MHVItemType* m_type;
-    NSDate* m_effectiveDate;
-}
 
-@property (readwrite, nonatomic, strong) MHVItemKey* key;
-@property (readwrite, nonatomic, strong) MHVItemType* type;
-@property (readwrite, nonatomic, strong) NSDate* effectiveDate;
+@property (readwrite, nonatomic, strong) MHVItemKey *key;
+@property (readwrite, nonatomic, strong) MHVItemType *type;
+@property (readwrite, nonatomic, strong) NSDate *effectiveDate;
 
 @end
 
 @interface MHVPendingItemCollection : MHVCollection
+
 @end
