508e4e4099dc3f69654abc9f7b18eee6ccf75313
Nate Malubay <namalu@microsoft.com>
2017-06-16 14:04:25 -0700

    Added Pods directory to project and added shared framework scheme for carthage support

---
+++ Example/build/Pods.build/Debug-iphonesimulator/HealthVault.build/DerivedSources/HealthVault_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char HealthVaultVersionString[];
+ extern const double HealthVaultVersionNumber;
+
+ const unsigned char HealthVaultVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:HealthVault  PROJECT:Pods-1" "\n";
+ const double HealthVaultVersionNumber __attribute__ ((used)) = (double)1.;
