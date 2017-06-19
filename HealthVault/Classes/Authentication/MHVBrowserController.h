a2d18faf29e4895c6bd06834ed92a13a09b32c64
Michael Burford <miburf@microsoft.com>
2017-06-19 07:34:40 -0700

    Change from UIWebView to WKWebView (#88)
    * Change from UIWebView to WKWebView
    
    * Fix review comments
    
    * Disable webview while hidden, so can't tap hidden button
    
    * fix merge in tests

--- HealthVault/Classes/Authentication/MHVBrowserController.h
+++ HealthVault/Classes/Authentication/MHVBrowserController.h
@@ -1,5 +1,5 @@
 //
-//  MHVBrowserAuthBrokerProtocol.h
+//  MHVBrowserController.h
 //  MHVLib
 //
 //  Copyright (c) 2017 Microsoft Corporation. All rights reserved.
@@ -7,28 +7,29 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 // http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@class MHVBrowserAuthBroker;
+@protocol MHVBrowserAuthBrokerProtocol;
 
-@class UIViewController;
+@interface MHVBrowserController : UIViewController
 
-NS_ASSUME_NONNULL_BEGIN
+/**
+ * Create a browser view controller
+ *
+ * @param authBroker the authentication handler, authBroker expects to be the WKNavigationDelegate for a WKWebView
+ */
+- (instancetype)initWithAuthBroker:(id<MHVBrowserAuthBrokerProtocol>)authBroker;
 
-@protocol MHVBrowserAuthBrokerProtocol <NSObject>
-
-- (void)authenticateWithViewController:(UIViewController *_Nullable)viewController
-                              startUrl:(NSURL *)startUrl
-                                endUrl:(NSURL *)endUrl
-                            completion:(void (^)(NSURL *_Nullable successUrl, NSError *_Nullable error))completion;
+- (instancetype)init __unavailable;
++ (instancetype)new __unavailable;
 
 @end
-
-NS_ASSUME_NONNULL_END
