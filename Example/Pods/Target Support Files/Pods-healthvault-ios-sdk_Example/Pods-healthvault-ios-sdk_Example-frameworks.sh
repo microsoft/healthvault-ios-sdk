e0e3c45839f914bda5cbc0df54a22a3d01e67183
Michael Burford <miburf@microsoft.com>
2017-06-27 08:20:20 -0700

    Add EncryptedCoreData & SQLCipher; add GetRecordOperations method (#100)
    * Add pod spec dependency
    
    * Add EncryptedCoreData & SQLCipher; add GetRecordOperations method
    
    * OfflineThingCache subspec

--- Example/Pods/Target Support Files/Pods-healthvault-ios-sdk_Example/Pods-healthvault-ios-sdk_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-healthvault-ios-sdk_Example/Pods-healthvault-ios-sdk_Example-frameworks.sh
@@ -89,10 +89,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/EncryptedCoreData/EncryptedCoreData.framework"
   install_framework "$BUILT_PRODUCTS_DIR/HealthVault/HealthVault.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SQLCipher/SQLCipher.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/EncryptedCoreData/EncryptedCoreData.framework"
   install_framework "$BUILT_PRODUCTS_DIR/HealthVault/HealthVault.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SQLCipher/SQLCipher.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
