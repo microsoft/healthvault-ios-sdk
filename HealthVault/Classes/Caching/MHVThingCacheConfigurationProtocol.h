89f7c1bf14279594bb1774c2de1d593e5b93f1b1
Michael Burford <miburf@microsoft.com>
2017-07-11 10:07:10 -0700

    fix copyright

--- HealthVault/Classes/Caching/MHVThingCacheConfigurationProtocol.h
+++ HealthVault/Classes/Caching/MHVThingCacheConfigurationProtocol.h
@@ -1,9 +1,20 @@
 //
-//  MHVThingCacheConfigurationProtocol.h
-//  Pods
+//  MHVThingCacheDatabaseProtocol.h
+//  MHVLib
 //
-//  Created by Michael Burford on 7/11/17.
+// Copyright (c) 2017 Microsoft Corporation. All rights reserved.
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #import <Foundation/Foundation.h>
@@ -19,7 +30,7 @@
  
  The default is empty and no caching.
  */
-@property (nonatomic, strong) NSArray<NSString *> *cacheTypeIds;
+@property (nonatomic, strong, nullable) NSArray<NSString *> *cacheTypeIds;
 
 /**
  A timer will sync the database with this time interval while the app is active
@@ -29,10 +40,10 @@
 @property (nonatomic, assign) NSInteger syncIntervalSeconds;
 
 /**
- Database to use for caching
+ Database to use for caching, allowing a custom database to be implemented
  
  The default is nil, so MHVThingCacheDatabase will be used
  */
-@property (nonatomic, strong) id<MHVThingCacheDatabaseProtocol> database;
+@property (nonatomic, strong, nullable) id<MHVThingCacheDatabaseProtocol> database;
 
 @end
