1d5d43657e8fab1d6bead01776117747da37d5e3
Nate Malubay <namalu@microsoft.com>
2017-07-24 13:12:08 -0700

    Cleaned up podspec for better file organization.

--- Example/Pods/Target Support Files/HealthVault-OfflineThingCache/HealthVault-OfflineThingCache-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HealthVault_OfflineThingCache : NSObject
-@end
-@implementation PodsDummy_HealthVault_OfflineThingCache
-@end
--- Example/Pods/Target Support Files/Pods-healthvault-ios-sdk_Example/Pods-healthvault-ios-sdk_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-healthvault-ios-sdk_Example/Pods-healthvault-ios-sdk_Example-frameworks.sh
@@ -90,12 +90,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/EncryptedCoreData/EncryptedCoreData.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/HealthVault-OfflineThingCache/HealthVault.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/HealthVault/HealthVault.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SQLCipher/SQLCipher.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/EncryptedCoreData/EncryptedCoreData.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/HealthVault-OfflineThingCache/HealthVault.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/HealthVault/HealthVault.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SQLCipher/SQLCipher.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
