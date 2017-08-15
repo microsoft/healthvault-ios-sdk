26cd331d848ab474f501db3c965d73fcc114f0c1
Mike Neville <mikenev@microsoft.com>
2017-08-15 14:12:29 -0700

    for building

--- HealthVault/Tools/codegen/codegen.sh
+++ HealthVault/Tools/codegen/codegen.sh
@@ -5,6 +5,7 @@ mkdir output
 # grab the latest swagger documentation
 curl https://hvc-dev-khvwus01.westus2.cloudapp.azure.com/swagger/docs/2.0-preview -o KHV.json
 
+
 # run the generator
 swagger-codegen generate -i ./KHV.json -l objc -o output -c ./swagger.config -t ./template --reserved-words-mappings id=identifier,description=descriptionText --import-mappings Time=MHVTime
 
