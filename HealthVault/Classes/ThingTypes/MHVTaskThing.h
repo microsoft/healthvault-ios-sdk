15110f2e057430f5724cc7dfc4661238e1e4a8f5
Michael Burford <miburf@microsoft.com>
2017-09-01 15:17:01 -0700

    Add taskKey to MHVTaskThing (#140)
    * Add taskKey to MHVTaskThing
    
    * Fix to match comment for ActionPlanTask

--- HealthVault/Classes/ThingTypes/MHVTaskThing.h
+++ HealthVault/Classes/ThingTypes/MHVTaskThing.h
@@ -51,6 +51,10 @@
 //
 @property (readwrite, nonatomic, strong) MHVTaskStatusEnum *status;
 //
+// (Optional) The task key a provider sets and maintains for a user's created task.
+//
+@property (readwrite, nonatomic, strong) NSString *taskKey;
+//
 // (Optional) For a task whose completion is tied to recording a specific health measurement or other piece of data, this indicates the item type to be recorded.
 //
 @property (readwrite, nonatomic, strong) NSUUID *taskType;
@@ -68,3 +72,4 @@
 @property (readwrite, nonatomic, strong) MHVUUID *associatedObjectiveIds;
 
 @end
+
